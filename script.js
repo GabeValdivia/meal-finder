f5c19e08605ac8f6e9a24b89d9845a021fd95358
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-17 17:43:34 -0600

    Add eventlistner for a new searchMeal function. Begin to write searchMeal function, clear out previous input, get search term. Test it all works!

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ mealsEl = document.getElementById('meals'),
 resultHeading = document.getElementById('result-heading'),
 single_meailEl = document.getElementById('single-meal');
 
+// Search meal and fetch from API
+function searchMeal(e) {
+	e.preventDefault();
+
+	// Clear single meal
+	single_meailEl.innerHTML = '';
+
+	// Get search term
+	const term = search.value;
+
+	console.log(term);
+}
+
+
 //Event Listeners
+submit.addEventListener('submit', searchMeal);
