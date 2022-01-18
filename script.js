81976258aab33287d73a54351e3e090a9095a2b8
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-17 19:16:18 -0600

    Output meal heading, region, description, and ingredients

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ submit = document.getElementById('submit'),
 random = document.getElementById('random'),
 mealsEl = document.getElementById('meals'),
 resultHeading = document.getElementById('result-heading'),
-single_meailEl = document.getElementById('single-meal');
+single_mealEl = document.getElementById('single-meal');
 
 // Search meal and fetch from API
 function searchMeal(e) {
 	e.preventDefault();
 
 	// Clear single meal
-	single_meailEl.innerHTML = '';
+	single_mealEl.innerHTML = '';
 
 	// Get search term
 	const term = search.value;
@@ -29,7 +29,7 @@ function searchMeal(e) {
 				mealsEl.innerHTML = data.meals.map(meal => `
 					<div class="meal">
 						<img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
-						<div class="meal-info" data-mealID="${meal.IdMeal}">
+						<div class="meal-info" data-mealID="${meal.idMeal}">
 							<h3>${meal.strMeal}</h3>
 						</div>
 					</div>
@@ -43,6 +43,66 @@ function searchMeal(e) {
 	}
 }
 
+// Fetch meal by id
+function getMealById(mealID) {
+	fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)	
+	.then(res => res.json())
+	.then(data => {
+		const meal = data.meals[0];
+
+		addMealToDOM(meal);
+	})
+}
+
+
+//Add meal to DOM
+function addMealToDOM(meal) {
+	const ingredients = [];
+
+	for(let i = 1; i <= 20; i++) {
+		if(meal[`strIngredient${i}`]){
+			ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+		} else {
+			break;
+		}
+	}
+
+	single_mealEl.innerHTML = `
+		<div class="single-meal">
+			<h1>${meal.strMeal}</h1>
+			<img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+			<div class="single-meal-info">
+				${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+				${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+			</div>
+			<div class="main">
+				<p>${meal.strInstructions}</p>
+				<h2>Ingredients</h2>
+				<ul>
+					${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+				</ul>
+			</div>
+		</div>
+	`;
+}
 
 //Event Listeners
 submit.addEventListener('submit', searchMeal);
+
+mealsEl.addEventListener('click', e => {
+	//path is supported in Chrome but not FF
+	const path = e.path || (e.composedPath && e.composedPath());
+
+	const mealInfo = path.find(item => {
+		if(item.classList){
+			return item.classList.contains('meal-info');
+		} else {
+			return false;
+		}
+	});
+
+	if(mealInfo){
+		const mealID = mealInfo.getAttribute('data-mealid');
+		getMealById(mealID);
+	}
+});
